08a465b20281603c53ecbae6bb9a84c97ec03072
Coffee Man <hectorscodingworld@gmail.com>
2020-09-19 10:58:24 -0400

    dice and score change
    dice and score change

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var scores, roundScore, activePlayer
 
 scores = [0, 0]
 roundScore = 0
-activePlayer = 1
+activePlayer = 0
 
 document.querySelector('.dice').style.display = 'none'
 
@@ -14,9 +14,31 @@ document.getElementById('current-1').textContent = '0'
 document.querySelector('.btn-roll').addEventListener('click', function btn() {
   // 1. random Number
   var dice = Math.floor(Math.random() * 6) + 1
+
   // 2. Display the results
   var diceDom = document.querySelector('.dice')
   diceDom.style.display = 'block'
   diceDom.src = '/image/dice-' + dice + '.png'
+
   //3. Update the round score IF thee rolled number was NOT a 1
+  if (dice > 1) {
+    //ADD score
+    roundScore += dice
+    document.querySelector('#current-' + activePlayer).textContent = roundScore
+  } else {
+    //Next player
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0)
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none'
+
+    // document.querySelector('.player-0-panel').classList.remove('active')
+    // document.querySelector('.player-1-panel').classList.add('active')
+  }
 })
